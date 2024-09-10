6c22378aa58f7a0e2956d77f17eec1569002d73f
mohamuhsin <muhsinohoho@outlook.com>
2024-09-10 23:45:14 +0300

    created the dummy products and mapped over them, made sure, id prop is on props ends

--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,8 +1,21 @@
-import Card from '../UI/Card';
-import classes from './ProductItem.module.css';
+import Card from "../UI/Card";
+import classes from "./ProductItem.module.css";
+import { useDispatch } from "react-redux";
+import { cartActions } from "../../store/cart-slice";
 
 const ProductItem = (props) => {
-  const { title, price, description } = props;
+  const dispatch = useDispatch();
+  const { title, price, description, id } = props;
+
+  function addItemToCart(item) {
+    dispatch(
+      cartActions.addItemToCart({
+        id,
+        item,
+        price,
+      })
+    );
+  }
 
   return (
     <li className={classes.item}>
@@ -13,7 +26,7 @@ const ProductItem = (props) => {
         </header>
         <p>{description}</p>
         <div className={classes.actions}>
-          <button>Add to Cart</button>
+          <button onClick={() => addItemToCart()}>Add to Cart</button>
         </div>
       </Card>
     </li>
