7c367c4b8166e1ddf27875fd542f34a9c01f925f
mohamuhsin <muhsinohoho@outlook.com>
2024-09-11 19:50:40 +0300

    rendred cartItems properly, passed all required props and fired the adding and removing buttons in the cart

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -10,9 +10,10 @@ const cartSlice = createSlice({
         addItemToCart(state, action) {
             const newItem = action.payload;
             const existingItem = state.items.find((item) => item.id === newItem.id);
+            state.totalQuantity++;
             if (!existingItem) {
                 state.items.push({
-                    itemId: newItem.id,
+                    id: newItem.id,
                     price: newItem.price,
                     quantity: 1,
                     totalPrice: newItem.price,
@@ -26,11 +27,12 @@ const cartSlice = createSlice({
         removeItemToCart(state, action) {
             const id = action.payload;
             const existingItem = state.items.find((item) => item.id === id);
-
+            state.totalQuantity--;
             if (existingItem.quantity === 1) {
                 state.items = state.items.filter((item) => item.id !== id);
             } else {
                 existingItem.quantity--;
+                existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
             }
         },
     },
