973fe2eca1e1c10ff203ce712906337c22308cc7
kelvinmacharia254 <pilotkelvin0@gmail.com>
2024-09-16 12:38:14 +0300

    - handle side effects of sending cart data to the firebase database, the http request code using fetchAPI is wrapped inside useEffect hook. - update notification state of the redux store depending on success or failure of the http request. - use notication state to conditionally render the Notification component - prevent the side effect from running on initial render using isInitial flag. - update readme

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # Redux Cart:
 
 This project demonstrates:
+
 > How to work with redux to manage app wide state, in this case updates made to the cart from various components within the app.
 > How to work with Side Effects and Asynchronous code in redux.
 
 # How We Built Step By Step:
-> We add the redux store with two slice, 'ui-slice' toggle visibility of the cart on the UI and 'cart-slice' to manage state of the cart.
-> 
-# How We Built
+
+> We added the redux store with two slices; 'ui-slice' to toggle visibility of the cart on the UI and the 'cart-slice' to manage state of the cart.
+
+# Handling side effects in redux
+> We wrapped asynchronous operation like http request using fetch API inside useEffect hook as it is the practice in functional components.
+> We dispatched actions to update the notification state depending on success or failure of the http request where we were
+  sending cart data to the firebase database. The notification component will use this state portion of the redux store to display appropriate message to the user.
+> We used isInitial flag to prevent fetch operation from running on the initial render of the app.
+
--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import { useEffect } from "react";
+import {Fragment, useEffect} from "react";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import { useSelector } from "react-redux";
+import Notification from "./components/UI/Notification";
+import { useSelector,useDispatch } from "react-redux";
+import {uiActions} from "./store/ui-slice";
 
+let isInitial = true
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    fetch("https://react-77315-default-rtdb.firebaseio.com/cart.json", {
-      method: "PUT",
-      body: JSON.stringify(cart),
-    });
-  }, [cart]);
+    async function sendCartData(){
+      dispatch(uiActions.showNotification({
+        status: "pending",
+        title: "sending",
+        message:"sending cart data"
+      }))
+      const response = await fetch("https://react-77315-default-rtdb.firebaseio.com/cart.json", {
+        method: "PUT",
+        body: JSON.stringify(cart),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to send cart!");
+      }
+
+      dispatch(uiActions.showNotification({
+        status: "Success",
+        title: "Success",
+        message:"Cart data sent successfully"
+      }))
+
+    }
+
+    if (isInitial){
+        isInitial = false
+        return
+  }
+
+    sendCartData().catch(error =>{
+      dispatch(uiActions.showNotification({
+        status:"error",
+        title: "Error",
+        message:"Failed to send cart data"
+      }))
+    })
+
+  }, [cart, dispatch]);
+
+
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+      <>
+        {notification && <Notification status={notification.status} title={notification.title} message={notification.message} />}
+        <Layout>
+          {showCart && <Cart/>}
+          <Products/>
+        </Layout>
+      </>
   );
 }
 
